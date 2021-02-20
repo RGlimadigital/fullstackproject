38cd3dc4eeb9b40709592980ac72e5f09cdb1f9c
AngelaAFrison <angelafrisonA@192.168.1.8>
2021-02-20 12:22:40 -0300

    New migrates table

--- backend/migrations/20210220073335_create_table_articles.js
+++ backend/migrations/20210220073335_create_table_articles.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.shcema.createTable('articles', table => {
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.string('description', 1000).notNull()
+        table.string('imageUrl', 1000).notNull()
+        table.binary('content').notNull()
+        table.integer('userId').refereces('id').inTable('users')
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+
 };

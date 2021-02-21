f6a03dc74495221e1331ebf55210dca0574ad2fb
RGlimadigital <RgFullDev@gmail.com>
2021-02-21 12:29:14 -0300

    Bug fixed

--- backend/api/user.js
+++ backend/api/user.js
@@ -4,37 +4,34 @@ module.exports = app => {
     const { existsOrError, notExistsOrError, equalsOrError, validEmail } = app.api.validation;
 
     const encryptPassword = password => {
-
         const salt = bcrypt.genSaltSync(10);
         return bcrypt.hashSync(password, salt);
-
     }
 
     const save = async(req, res) => {
         const user = {...req.body };
-        console.log(user)
         if (req.params.id) user.id = req.params.id;
 
         try {
-            existsOrError(user.name, 'Nome de usuário não informado.');
-            existsOrError(user.email, 'E-mail de usuário nao informado.')
-            existsOrError(user.password, 'Senha não informada');
-            existsOrError(user.confirmPassword, 'Confirmação de senha não encontrada');
+            existsOrError(user.name, 'Nome não informado')
+            existsOrError(user.email, 'E-mail não informado')
+            existsOrError(user.password, 'Senha não informada')
+            existsOrError(user.confirmPassword, 'Confirmação de Senha inválida')
+            equalsOrError(user.password, user.confirmPassword,
+                'Senhas não conferem')
 
-            console.log("entrou aqui")
-            const userFromDb = await app.db('users')
+            const userFromDB = await app.db('users')
                 .where({ email: user.email }).first()
-
             if (!user.id) {
-                notExistsOrError(userFromDb, 'Usuário já cadastrado.')
+                console.log("teste de email")
+                notExistsOrError(userFromDB, 'Usuário já cadastrado')
             }
         } catch (msg) {
-            return res.status(400).send(msg);
+            return res.status(400).send(msg)
         }
-
         user.password = encryptPassword(user.password);
         delete user.confirmPassword;
-        console.log(user.password)
+
 
         if (user.id) {
             app.db('users')
@@ -46,7 +43,7 @@ module.exports = app => {
             app.db('users')
                 .insert(user)
                 .then(_ => res.status(204).send())
-                .catch(err => res.status(500).send(err))
+                .catch(err => res.status(500).send('Entrou aqui'))
         }
     }
 
--- backend/api/validation.js
+++ backend/api/validation.js
@@ -1,18 +1,17 @@
 module.exports = app => {
     function existsOrError(value, msg) {
-        console.log(value)
         if (!value) throw msg;
         if (Array.isArray(value) && value.length === 0) throw msg;
-        if (typeof value === 'string' & !value.trim()) throw msg;
+        if (typeof value === 'string' && !value.trim()) throw msg;
     }
 
     function notExistsOrError(value, msg) {
         try {
-            existsOrError(value, msg);
-        } catch (msg) {
-            return msg;
-        }
 
+            existsOrError(value, msg)
+        } catch (msg) {
+            return;
+        }
         throw msg;
     }
 
@@ -20,10 +19,7 @@ module.exports = app => {
         if (valueA !== valueB) throw msg;
     }
 
-    function validEmail(email) {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
 
-    return { existsOrError, notExistsOrError, equalsOrError, validEmail };
+
+    return { existsOrError, notExistsOrError, equalsOrError };
 }
